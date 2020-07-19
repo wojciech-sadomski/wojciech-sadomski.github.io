de0cd857670e7a03405911911888985d0d2520b2
Wojciech Sadomski <wsadomski@gmail.com>
2020-07-19 11:10:03 +0200

    feat: js respository get, clear

--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@
 ## Czego się dowiesz na stronie
 
 Jestem inżynierem-konstruktorem z kilkunastoletnim stażem, aktualnie chciałbym zmienić branżę i rozpocząć dalszy rozwój w kierunku IT.
-Ten projekt pozwoli w przystępny sposób, pokazać, co już potrafię a co chciałbym jeszcze rozwinąć.
+Ten projekt pozwoli w przystępny sposób, pokazać, co już potrafię a co chciałbym jeszcze rozwinąć. Wszystkie wymienione umiejętności staram się stale doskonalić.
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,52 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("Witam serdecznie! Miło Cię widzieć! 🙂");
+
+fetch("https://api.github.com/users/wojciech-sadomski/repos")
+  .then((res) => res.json())
+  .then((res) => {
+    const container = document.querySelector(".projects-grid--js");
+    console.log(res);
+    for (let repo of res) {
+      const { description, homepage, html_url, name } = repo;
+      const template = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="../public/img/Github-Icon.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">Project:</span
+            ><span>${name}</span>
+          </h3>
+          <p class="project__grid">
+            <span class="project__label">Descryption: </span
+            ><span>${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">Demo: </span>
+            <span>
+              &lt;<a href="${homepage}" title=${name} class="project__link"
+                >see more</a
+              >&gt;
+            </span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">Github: </span>
+            <span>
+              &lt;<a href="${html_url}"
+              title=${name}
+              class="project__link"
+                >surce code</a
+              >&gt;
+            </span>
+          </p>
+        </div>
+      </article>`;
+      container.innerHTML += template;
+    }
+  })
+  .catch((e) => console.log(e));
